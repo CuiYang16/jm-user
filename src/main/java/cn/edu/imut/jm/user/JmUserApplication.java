f5f613e577c42b8cdefc8f79fb5259c9825493e5
CuiYang16 <cuiyangmail@163.com>
2019-04-10 09:39:53 +0800

    first

---
+++ src/main/java/cn/edu/imut/jm/user/JmUserApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.imut.jm.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmUserApplication.class, args);
+	}
+
+}
